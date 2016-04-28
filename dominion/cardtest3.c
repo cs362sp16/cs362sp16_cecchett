83c8799c2cf59045fe9725fcfd8ffac8e7862dbb
t-a-n-n-e-r <a@a.com>
2016-04-28 04:34:08 -0700

    Add card tests for Smithy and Council Room

--- dominion/cardtest3.c
+++ dominion/cardtest3.c
@@ -3,11 +3,26 @@
 #include "dominion.h"
 
 int main() {
-	start("");
+	start("cardEffect (Smithy)");
 
 	struct gameState g;
 
-	
+	int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+	       outpost,baron,sea_hag};
+
+  	int r = initializeGame(4, k, 5, &g);
+  	assert(r == 0, "Game initialized.");
+
+  	// let's mock some stuff
+  	g.whoseTurn = 0;
+  	g.hand[0][0] = smithy;
+
+  	int handCountBefore = g.handCount[0];
+
+  	playCard(0, -1, -1, -1, &g);
+
+  	assert(g.handCount[0] = handCountBefore + 2, "Player draws three cards (hand count += 2, since Smithy is removed).");
+  	assert(g.hand[0][0] != smithy, "Player's smithy card is removed.");
 
 	end();
 }
--- dominion/cardtest4.c
+++ dominion/cardtest4.c
@@ -3,11 +3,32 @@
 #include "dominion.h"
 
 int main() {
-	start("");
+	start("cardEffect (Council Room)");
 
 	struct gameState g;
 
-	
+	int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+	       outpost,baron,council_room};
+
+  	int r = initializeGame(4, k, 5, &g);
+  	assert(r == 0, "Game initialized.");
+
+  	// mock the scenario
+  	g.whoseTurn = 0;
+  	g.hand[0][0] = council_room;
+  	g.handCount[0] = 1;
+
+  	g.handCount[1] = 0;
+  	g.handCount[2] = 1;
+
+  	int numBuysBefore = g.numBuys;
+
+  	playCard(0, -1, -1, -1, &g);
+
+  	assert(g.numBuys == numBuysBefore + 1, "Number of buys is increased by 1.");
+  	assert(g.handCount[0] == 4, "Player draws 4 cards.");
+  	assert(g.handCount[1] == 1, "Another player draws 1 card. (No cards in hand)");
+  	assert(g.handCount[2] == 2, "Another player draws 1 card. (One card in hand)");
 
 	end();
 }
